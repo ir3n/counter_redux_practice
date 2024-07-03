d8b46a177daf92bcce2fe9b0c27151af270d80b0
ir3n <iren.borada@gmail.com>
2024-07-03 15:44:28 +0200

    Styling: Adds Redux icon & uses its color throughout the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Counter from "@/components/Counter";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -7,7 +8,14 @@ export default function Home() {
         This is a meaningless counter
       </h1>
       <h2 className=" text-2xl lg:text-4xl text-center mb-10">
-        But it works with <strong>Redux</strong>
+        But it works with <strong className="text-redux">Redux</strong>
+        <Image
+          src="redux.svg"
+          alt="Redux"
+          width={40}
+          height={40}
+          className="inline"
+        />
       </h2>
 
       <Counter />
--- components/Counter/CounterButton.tsx
+++ components/Counter/CounterButton.tsx
@@ -6,7 +6,7 @@ function CounterButton({ action, fn }) {
   return (
     <button
       onClick={fn}
-      className="w-12 h-12 md:w-16 md:h-16 bg-indigo-500 rounded-xl text-white flex items-center justify-center text-4xl cursor-pointer"
+      className="w-12 h-12 md:w-16 md:h-16 bg-redux rounded-xl text-white flex items-center justify-center text-4xl cursor-pointer"
     >
       <span className="pointer-events-none">
         {action === "plus" ? "+" : "-"}
