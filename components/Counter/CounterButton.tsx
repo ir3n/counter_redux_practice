267a6b2bac552d7d1b3cb1a814f8c07d5626b23c
ir3n <iren.borada@gmail.com>
2024-07-03 21:29:52 +0200

    Style: Hover on buttons

--- components/Counter/CounterButton.tsx
+++ components/Counter/CounterButton.tsx
@@ -6,7 +6,7 @@ function CounterButton({ action, fn }) {
   return (
     <button
       onClick={fn}
-      className="w-12 h-12 md:w-16 md:h-16 bg-redux rounded-xl text-white flex items-center justify-center text-4xl cursor-pointer"
+      className="w-12 h-12 md:w-16 md:h-16 bg-redux rounded-xl text-white flex items-center justify-center text-4xl cursor-pointer lg:hover:scale-110 transition duration-500"
     >
       <span className="pointer-events-none">
         {action === "plus" ? "+" : "-"}
